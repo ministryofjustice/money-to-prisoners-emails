6352dd5a5a202ff621004197fbb366aa618fd6b5
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-09-03 15:47:50 +0100

    Validate GOV.UK Notify callback: connection must be secure and bearer token must match

--- mtp_emails/apps/callbacks/tests/test_notify_callbacks.py
+++ mtp_emails/apps/callbacks/tests/test_notify_callbacks.py
@@ -1,19 +1,57 @@
 from http import HTTPStatus
 import logging
+from unittest import mock
 
-from django.test import SimpleTestCase
+from django.http import HttpRequest
+from django.test import SimpleTestCase, override_settings
 from django.urls import reverse_lazy
 from mtp_common.test_utils import silence_logger
 
 
+@override_settings(GOVUK_NOTIFY_CALLBACKS_BEARER_TOKEN='0000111122223333')
 class NotifyCallbacksTestCase(SimpleTestCase):
     url = reverse_lazy('callbacks')
 
     def test_callback_must_be_posted(self):
         with silence_logger(name='django.request', level=logging.ERROR):
             response = self.client.get(self.url)
-        self.assertNotContains(response, 'placeholder', status_code=HTTPStatus.METHOD_NOT_ALLOWED)
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+        self.assertIn('POST', response.get('Allow'))
 
-    def test_callback_accepted(self):
-        response = self.client.post(self.url)
-        self.assertContains(response, 'placeholder', status_code=HTTPStatus.OK)
+    def assertRejectedCallback(self, response):  # noqa: N802
+        self.assertContains(response, 'Invalid request', status_code=HTTPStatus.BAD_REQUEST)
+
+    def assertAcceptedCallback(self, response):  # noqa: N802
+        self.assertNotContains(response, 'Invalid request', status_code=HTTPStatus.NO_CONTENT)
+        self.assertEqual(response.content, b'')
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_insecure_callback_rejected(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = False
+
+        with silence_logger(name='django.request', level=logging.ERROR):
+            response = self.client.post(self.url, HTTP_AUTHORIZATION='Bearer 0000111122223333')
+        self.assertRejectedCallback(response)
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_secure_callback_with_missing_token_rejected(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        with silence_logger(name='django.request', level=logging.ERROR):
+            response = self.client.post(self.url)
+        self.assertRejectedCallback(response)
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_secure_callback_with_invalid_token_rejected(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        with silence_logger(name='django.request', level=logging.ERROR):
+            response = self.client.post(self.url, HTTP_AUTHORIZATION='Bearer wrong-token')
+        self.assertRejectedCallback(response)
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_secure_callback_with_valid_token_accepted(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        response = self.client.post(self.url, HTTP_AUTHORIZATION='Bearer 0000111122223333')
+        self.assertAcceptedCallback(response)
--- mtp_emails/apps/callbacks/urls.py
+++ mtp_emails/apps/callbacks/urls.py
@@ -1,12 +1,39 @@
-from django.http import HttpResponse
+from http import HTTPStatus
+
+from django.conf import settings
+from django.http import HttpResponse, HttpRequest
 from django.urls import path
+from django.utils.crypto import constant_time_compare
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
 
+class InvalidRequest(Exception):
+    pass
+
+
 class NotifyCallbackView(View):
     def post(self, request):
-        return HttpResponse('TODO: This is just a placeholder/test')
+        try:
+            self.validate_request()
+        except InvalidRequest as e:
+            return HttpResponse(
+                f'Invalid request: {e}',
+                content_type='text/plain',
+                status=HTTPStatus.BAD_REQUEST,
+            )
+        return HttpResponse(b'', status=HTTPStatus.NO_CONTENT)
+
+    def validate_request(self):
+        request: HttpRequest = self.request
+        if not request.is_secure():
+            raise InvalidRequest('Insecure connection')
+        auth_header = request.headers.get('Authorization')
+        if not auth_header or not auth_header.startswith('Bearer '):
+            raise InvalidRequest('Missing authorisation bearer token')
+        bearer_token = auth_header[len('Bearer '):]
+        if not constant_time_compare(bearer_token, settings.GOVUK_NOTIFY_CALLBACKS_BEARER_TOKEN):
+            raise InvalidRequest('Invalid bearer token')
 
 
 urlpatterns = [
