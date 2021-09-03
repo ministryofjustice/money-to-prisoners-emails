389e3a40806966447dc35358e072ac2d779b1422
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-09-03 16:55:01 +0100

    Parse GOV.UK Notify callback payload and categorise its type

--- mtp_emails/apps/callbacks/tests/test_notify_callbacks.py
+++ mtp_emails/apps/callbacks/tests/test_notify_callbacks.py
@@ -12,18 +12,34 @@ from mtp_common.test_utils import silence_logger
 class NotifyCallbacksTestCase(SimpleTestCase):
     url = reverse_lazy('callbacks')
 
-    def test_callback_must_be_posted(self):
-        with silence_logger(name='django.request', level=logging.ERROR):
-            response = self.client.get(self.url)
-        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
-        self.assertIn('POST', response.get('Allow'))
+    valid_delivery_receipt_payload = {
+        # NB: this is not an exhaustive set of valid fields
+        'id': '11111-22222', 'sent_at': '2021-09-03T12:15:30Z',
+        'notification_type': 'email', 'template_id': '11',
+        'reference': 'transaction-111112',
+        'to': 'user@outside.local',
+        'status': 'delivered',
+    }
+    valid_received_text_message_payload = {
+        'id': '11111-22222', 'date_received': '2021-09-03T12:15:30Z',
+        'source_number': '07000000000', 'destination_number': '07000000001', 'message': 'How do I sign in?',
+    }
 
-    def assertRejectedCallback(self, response):  # noqa: N802
-        self.assertContains(response, 'Invalid request', status_code=HTTPStatus.BAD_REQUEST)
+    def assertRejectedCallback(self, response, expected_error):  # noqa: N802
+        self.assertContains(response, expected_error, status_code=HTTPStatus.BAD_REQUEST)
 
     def assertAcceptedCallback(self, response):  # noqa: N802
         self.assertNotContains(response, 'Invalid request', status_code=HTTPStatus.NO_CONTENT)
         self.assertEqual(response.content, b'')
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_callback_must_be_posted(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        with silence_logger(name='django.request', level=logging.ERROR):
+            response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+        self.assertIn('POST', response.get('Allow'))
 
     @mock.patch.object(HttpRequest, 'is_secure')
     def test_insecure_callback_rejected(self, mock_request_is_secure):
@@ -31,7 +47,7 @@ class NotifyCallbacksTestCase(SimpleTestCase):
 
         with silence_logger(name='django.request', level=logging.ERROR):
             response = self.client.post(self.url, HTTP_AUTHORIZATION='Bearer 0000111122223333')
-        self.assertRejectedCallback(response)
+        self.assertRejectedCallback(response, 'Insecure connection')
 
     @mock.patch.object(HttpRequest, 'is_secure')
     def test_secure_callback_with_missing_token_rejected(self, mock_request_is_secure):
@@ -39,7 +55,7 @@ class NotifyCallbacksTestCase(SimpleTestCase):
 
         with silence_logger(name='django.request', level=logging.ERROR):
             response = self.client.post(self.url)
-        self.assertRejectedCallback(response)
+        self.assertRejectedCallback(response, 'Missing authorisation bearer token')
 
     @mock.patch.object(HttpRequest, 'is_secure')
     def test_secure_callback_with_invalid_token_rejected(self, mock_request_is_secure):
@@ -47,11 +63,61 @@ class NotifyCallbacksTestCase(SimpleTestCase):
 
         with silence_logger(name='django.request', level=logging.ERROR):
             response = self.client.post(self.url, HTTP_AUTHORIZATION='Bearer wrong-token')
-        self.assertRejectedCallback(response)
+        self.assertRejectedCallback(response, 'Invalid bearer token')
 
     @mock.patch.object(HttpRequest, 'is_secure')
-    def test_secure_callback_with_valid_token_accepted(self, mock_request_is_secure):
+    def test_secure_callback_with_invalid_content_type_rejected(self, mock_request_is_secure):
         mock_request_is_secure.return_value = True
 
-        response = self.client.post(self.url, HTTP_AUTHORIZATION='Bearer 0000111122223333')
+        with silence_logger(name='django.request', level=logging.ERROR):
+            response = self.client.post(
+                self.url, data=self.valid_delivery_receipt_payload,
+                HTTP_AUTHORIZATION='Bearer 0000111122223333',
+            )
+        self.assertRejectedCallback(response, 'Invalid content type')
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_secure_callback_with_invalid_payload_encoding_rejected(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        with silence_logger(name='django.request', level=logging.ERROR):
+            response = self.client.post(
+                self.url, data='id=11111-22222',
+                content_type='application/json',
+                HTTP_AUTHORIZATION='Bearer 0000111122223333',
+            )
+        self.assertRejectedCallback(response, 'Invalid JSON payload')
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_secure_callback_with_invalid_json_payload_rejected(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        with silence_logger(name='django.request', level=logging.ERROR):
+            response = self.client.post(
+                self.url, data={'id': '11111-22222'},
+                content_type='application/json',
+                HTTP_AUTHORIZATION='Bearer 0000111122223333',
+            )
+        self.assertRejectedCallback(response, 'JSON payload is not a known callback type')
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_secure_delivery_receipt_with_valid_token_accepted(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        response = self.client.post(
+            self.url, data=self.valid_delivery_receipt_payload,
+            content_type='application/json',
+            HTTP_AUTHORIZATION='Bearer 0000111122223333',
+        )
+        self.assertAcceptedCallback(response)
+
+    @mock.patch.object(HttpRequest, 'is_secure')
+    def test_secure_received_text_message_with_valid_token_accepted(self, mock_request_is_secure):
+        mock_request_is_secure.return_value = True
+
+        response = self.client.post(
+            self.url, data=self.valid_received_text_message_payload,
+            content_type='application/json',
+            HTTP_AUTHORIZATION='Bearer 0000111122223333',
+        )
         self.assertAcceptedCallback(response)
--- mtp_emails/apps/callbacks/urls.py
+++ mtp_emails/apps/callbacks/urls.py
@@ -1,3 +1,5 @@
+import enum
+import json
 from http import HTTPStatus
 
 from django.conf import settings
@@ -12,10 +14,26 @@ class InvalidRequest(Exception):
     pass
 
 
+class CallbackType(enum.Enum):
+    """
+    Defined the fields GOV.UK Notify will include in a callback.
+    NB: lists of fields are not exhaustive – includes only fields used to distinguish between callback types
+    """
+    delivery_receipt = ('id', 'reference', 'to', 'status', 'notification_type', 'sent_at', 'template_id')
+    received_text_message = ('id', 'source_number', 'destination_number', 'message', 'date_received')
+
+    @classmethod
+    def get_payload_type(cls, payload):
+        for callback_type in cls:
+            if all(field in payload for field in callback_type.value):
+                return callback_type
+
+
 class NotifyCallbackView(View):
     def post(self, request):
         try:
             self.validate_request()
+            callback_type, payload = self.parse_callback()
         except InvalidRequest as e:
             return HttpResponse(
                 f'Invalid request: {e}',
@@ -34,6 +52,21 @@ class NotifyCallbackView(View):
         bearer_token = auth_header[len('Bearer '):]
         if not constant_time_compare(bearer_token, settings.GOVUK_NOTIFY_CALLBACKS_BEARER_TOKEN):
             raise InvalidRequest('Invalid bearer token')
+
+    def parse_callback(self) -> tuple:
+        request: HttpRequest = self.request
+        if request.content_type != 'application/json':
+            raise InvalidRequest('Invalid content type')
+        try:
+            payload = json.loads(request.body)
+            if not isinstance(payload, dict):
+                raise TypeError
+        except (ValueError, TypeError):
+            raise InvalidRequest('Invalid JSON payload')
+        callback_type = CallbackType.get_payload_type(payload)
+        if not callback_type:
+            raise InvalidRequest('JSON payload is not a known callback type')
+        return callback_type, payload
 
 
 urlpatterns = [
